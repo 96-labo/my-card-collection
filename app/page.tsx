1037dfaa433622ee1774d74928c3ef5b65a5c375
96-labo <96-labo@example.com>
2026-01-31 11:23:10 +0900

    feat: fortune gimmick on icon click

--- app/page.tsx
+++ app/page.tsx
@@ -340,7 +340,7 @@ return (
         <MoreHorizontal size={24} />
       </div>
 
-      <div className="flex items-center gap-2">
+      <div className="flex items-center">
       <button 
   onClick={() => {
     enableSensor(); // ここでセンサーを叩き起こす！
