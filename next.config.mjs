baf72202366fa3b39190e32dec559ffd77977002
96-labo <96-labo@example.com>
2026-01-31 10:20:15 +0900

    feat: fortune gimmick on icon click

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-import withPWAInit from 'next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // 開発環境では無効化してエラーを防ぐ
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ここに本来の設定（もしあれば）を書く
+  // 一旦空っぽにする
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
